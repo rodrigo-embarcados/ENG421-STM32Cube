d428d4e4990403e2d239b6a52d1e4098104184b1
rodrigo-embarcados <rodrigo.rdvr@gmail.com>
2025-05-10 23:49:11 -0300

    Corrigindo arquivos Aula_6b

--- Aula_6b/Core/Src/main.c
+++ Aula_6b/Core/Src/main.c
@@ -259,7 +259,7 @@ static void conf_TIM2(void)
 static void conf_TIM3(void)
 {
 	__HAL_RCC_TIM3_CLK_ENABLE();
-    htim3.Instance = TIM3;
+        htim3.Instance = TIM3;
 	htim3.Init.Prescaler = 7200 - 1;
 	htim3.Init.Period = 20000 - 1;
 	htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
